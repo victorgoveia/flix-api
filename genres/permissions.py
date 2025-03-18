8b0273877d47ec49de256ebba17ef24ef2ea543f
Victor Goveia <victor.goov@gmail.com>
2025-03-18 14:52:12 -0300

    created permissions.py in genres and included GenrePermissionClass in views.py

---
+++ genres/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework import permissions
+
+
+class GenrePermissionClass(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method == 'GET':
+            return request.user.has_perm('genres.view_genre') 
+        return False
+        
