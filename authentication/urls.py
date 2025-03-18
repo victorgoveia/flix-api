900e019498d9f2916fee8dc28d897e31ef5d8fcb
Victor Augusto Gouveia da Rocha <victor.rocha@cortelsp.com.br>
2025-03-18 08:54:21 -0300

    removed unecessary files and created url authentication/token in authentication/urls.py

---
+++ authentication/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+urlpatterns = [
+    path('authentication/token/', TokenObtainPairView.as_view(), name='token-obtain-pair')]
