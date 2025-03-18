3b2a056690782c7da583b6a3bedc4f7b499fbeaa
Victor Augusto Gouveia da Rocha <victor.rocha@cortelsp.com.br>
2025-03-18 09:55:31 -0300

    added IsAuthenticated permission from rest_framework in all views.py files

--- movies/views.py
+++ movies/views.py
@@ -1,12 +1,15 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from movies.models import Movies
 from movies.serializers import MovieSerializer
 
 
 class MovieCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,) 
     queryset = Movies.objects.all()
     serializer_class = MovieSerializer
 
 class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,) 
     queryset = Movies.objects.all()
     serializer_class = MovieSerializer
