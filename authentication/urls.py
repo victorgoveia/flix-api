f0c51d77140a6d8969bff0ca53840a423fa0f18a
Victor Augusto Gouveia da Rocha <victor.rocha@cortelsp.com.br>
2025-03-18 11:08:09 -0300

    added refresh and verify token from simplejwt

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,21 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
 
 urlpatterns = [
-    path('authentication/token/', TokenObtainPairView.as_view(), name='token-obtain-pair')]
+    path(
+        "authentication/token/", TokenObtainPairView.as_view(), name="token-obtain-pair"
+    ),
+    path(
+        "authentication/token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token-refresh",
+    ),
+    path(
+        "authentication/token/verify/", TokenVerifyView.as_view(), name="token-verify"
+    ),
+]
