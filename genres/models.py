d449b5501995d3d79a6f15786afff9d4051b0570
Victor Goveia <victor.goov@gmail.com>
2025-04-24 14:48:48 -0300

    added verbose name in model Genre name

--- genres/models.py
+++ genres/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, verbose_name="Nome")
     active = models.BooleanField(default=True, verbose_name="Ativo")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Criado em")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Atualizado em")
